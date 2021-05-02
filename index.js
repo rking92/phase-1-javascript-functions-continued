2b81d64946e6d7e73afd8d58dfa867670986848d
Robert <rking92@gmail.com>
2021-05-01 22:23:42 -0400

    commit tree 08370f8d5feb794fdc29b2ca6bacf47f4936e6bf

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function saturdayFun (activity) {
+    activity = typeof activity !== 'undefined' ? activity : 'roller-skate';
+    return (`This Saturday, I want to ${activity}!`)
+    
+}
+ function mondayWork (job) {
+     job = typeof job !== 'undefined' ? job : 'go to the office'
+     return (`This Monday, I will ${job}.`)
+ }
+ function wrapAdjective(parameter ="*") {
+     return function innerFunction(msg='special') {
+        return (`You are ${parameter}${msg}${parameter}!`)
+ }
+}
